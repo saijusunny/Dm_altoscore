2db5ee8ffb0a3399b4dba76a510f1f1e839fd2f6
Saiju Sunny <saijusunny1301@gmail.com>
2023-03-10 18:24:48 +0530

    add dash

--- DMcoreapp/urls.py
+++ DMcoreapp/urls.py
@@ -10,8 +10,15 @@ urlpatterns = [
     #---------------------------------------------------------------------------Admin Section
     path('ad_base', views.ad_base, name='ad_base'),
     path('ad_profile', views.ad_profile, name='ad_profile'),
+    path('ad_dashboard', views.ad_dashboard, name='ad_dashboard'),
+    path('ad_create_work', views.ad_create_work, name='ad_create_work'),
+    path('ad_view_work', views.ad_view_work, name='ad_view_work'),
+    path('ad_view_clint', views.ad_view_clint, name='ad_view_clint'),
+    path('ad_daily_work', views.ad_daily_work, name='ad_daily_work'),
+    path('ad_daily_work_det', views.ad_daily_work_det, name='ad_daily_work_det'),
     #---------------------------------------------------------------------------Executive Section
     path('ex_base', views.ex_base, name='ex_base'),
     path('ex_profile', views.ex_profile, name='ex_profile'),
+    path('ex_dashboard', views.ex_dashboard, name='ex_dashboard'),
     
 ]
--- DMcoreapp/views.py
+++ DMcoreapp/views.py
@@ -24,6 +24,25 @@ def ad_base(request):
 def ad_profile(request):
     return render(request, 'admin/ad_profile.html')
 
+def ad_dashboard(request):
+    return render(request, 'admin/ad_dashboard.html')
+
+def ad_create_work(request):
+    return render(request, 'admin/ad_create_work.html')
+
+def ad_view_work(request):
+    return render(request, 'admin/ad_view_work.html')
+
+def ad_view_clint(request):
+    return render(request, 'admin/ad_view_clint.html')
+
+
+def ad_daily_work(request):
+    return render(request, 'admin/ad_daily_work.html')
+
+def ad_daily_work_det(request):
+    return render(request, 'admin/ad_daily_work_det.html')
+
 # -----------------------------------------------------------------------------Executive Section
 
 def ex_base(request):
@@ -31,3 +50,6 @@ def ex_base(request):
 
 def ex_profile(request):
     return render(request, 'executive/ex_profile.html')
+
+def ex_dashboard(request):
+    return render(request, 'executive/ex_dashboard.html')
